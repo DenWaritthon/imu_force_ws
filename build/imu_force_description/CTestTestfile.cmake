4a13a015e10029add4ea066eafd29c0b4aa56d70
waritt <waritt10@gmail.com>
2024-11-11 17:52:19 +0700

    set_pkg

---
+++ build/imu_force_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/waritt/imu_force_ws/src/imu_force_description
+# Build directory: /home/waritt/imu_force_ws/build/imu_force_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/imu_force_gazebo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/waritt/imu_force_ws/src/imu_force_gazebo
+# Build directory: /home/waritt/imu_force_ws/build/imu_force_gazebo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
