8b6acee233adb8008f308779182b1161744d1842
waritt <waritt10@gmail.com>
2024-12-24 15:10:50 +0700

    new_gazebo

---
+++ src/object_simulation/launch/model_display.launch.py
@@ -0,0 +1,53 @@
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+import os
+
+def generate_launch_description():
+
+    package_name = "object_simulation"
+    
+    rsp = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(
+                    get_package_share_directory(package_name),
+                    "launch",
+                    "model_description.launch.py"
+                )
+            ]
+        ),
+        launch_arguments={"use_sim_time":"true"}.items()
+    )
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(
+                    get_package_share_directory("gazebo_ros"),
+                    "launch",
+                    "gazebo.launch.py"
+                )
+            ]
+        )
+    )
+
+    spawn_entity = Node(
+        package="gazebo_ros",
+        executable="spawn_entity.py",
+        arguments=[
+            "-topic", "robot_description",
+            "-entity", "example"
+        ],
+        output = "screen"
+    )
+
+    launch_description = LaunchDescription()
+    launch_description.add_action(rsp)
+    launch_description.add_action(gazebo)
+    launch_description.add_action(spawn_entity)
+
+    return launch_description
--- src/object_simulation/scripts/dummy_script.py
+++ src/object_simulation/scripts/dummy_script.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from imu_filter.dummy_module import dummy_function, dummy_var
+from object_simulation.dummy_module import dummy_function, dummy_var
 import rclpy
 from rclpy.node import Node
 
